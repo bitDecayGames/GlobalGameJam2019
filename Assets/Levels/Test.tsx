12106c4c14ee44053500242b1c9392a6a5b6faa7
nandemoguy <nandemoguy@gmail.com>
2019-01-26 12:13:11 -0700

    art update

--- Assets/Levels/Test.tsx
+++ Assets/Levels/Test.tsx
@@ -13,7 +13,7 @@
  </tile>
  <tile id="2">
   <properties>
-   <property name="ObjectName" value="Ladder"/>
+   <property name="ObjectName" value="ladder"/>
   </properties>
  </tile>
  <tile id="3">
---
+++ Assets/Levels/Test2.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Test2" tilewidth="16" tileheight="16" tilecount="192" columns="12">
+ <image source="Sprites/Test2.png" width="192" height="256"/>
+ <tile id="36">
+  <properties>
+   <property name="ObjectName" value="player1Spawn"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="ObjectName" value="player2Spawn"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="ObjectName" value="Player3Spawn"/>
+  </properties>
+ </tile>
+ <tile id="39">
+  <properties>
+   <property name="ObjectName" value="player4Spawn"/>
+  </properties>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="ObjectName" value="ladder"/>
+  </properties>
+ </tile>
+</tileset>
