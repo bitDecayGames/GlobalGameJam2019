87a8a9313ecc5021d54da5aba5e15b2004d413ee
nandemoguy <nandemoguy@gmail.com>
2019-01-25 23:24:18 -0700

    Test Level update

--- Assets/Levels/Test.tsx
+++ Assets/Levels/Test.tsx
@@ -1,4 +1,29 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.1" name="TEst" tilewidth="16" tileheight="16" tilecount="100" columns="10">
  <image source="Sprites/Test.png" width="160" height="160"/>
+ <tile id="0">
+  <properties>
+   <property name="ObjectName" value=""/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="ObjectName" value=""/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="ObjectName" value="Ladder"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="ObjectName" value="door"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="ObjectName" value="button"/>
+  </properties>
+ </tile>
 </tileset>
