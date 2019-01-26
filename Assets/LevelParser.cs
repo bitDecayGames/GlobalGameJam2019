93251a68315b07ac85906088dc1bb7d20aba41b7
Tristan Havelick <tristan@havelick.com>
2019-01-26 12:12:04 -0700

    Extract spawn method

--- Assets/LevelParser.cs
+++ Assets/LevelParser.cs
@@ -8,74 +8,75 @@ using UnityEngine;
 public class LevelParser : MonoBehaviour
 {
 
-	public GameObject playerTemplate;
-	
-	// Use this for initialization
-	void Start ()
-	{
-		var mapScript = FindObjectOfType<SuperMap>();
-		if (mapScript == null)
-		{
-			throw new RuntimeException("There is no map in the scene");
-		}
+  public GameObject playerTemplate;
 
-		var objLayers = mapScript.gameObject.GetComponentsInChildren<SuperObjectLayer>();
-		if (objLayers == null || objLayers.Length < 1)
-		{
-			throw new RuntimeException("No object layers found in map object");
-		}
-		
-		foreach (var ol in objLayers)
-		{
-			if (ol.m_TiledName == "Object Layer 1")
-			{
-				parseLevelObjects(ol);
-			}
-		}
-	}
+  // Use this for initialization
+  void Start ()
+  {
+    var mapScript = FindObjectOfType<SuperMap>();
+    if (mapScript == null)
+    {
+      throw new RuntimeException("There is no map in the scene");
+    }
 
-	void parseLevelObjects(SuperObjectLayer objLayer)
-	{
-		Debug.Log("We are parsing an object layer");
-		var objects = objLayer.gameObject.GetComponentsInChildren<SuperObject>();
+    var objLayers = mapScript.gameObject.GetComponentsInChildren<SuperObjectLayer>();
+    if (objLayers == null || objLayers.Length < 1)
+    {
+      throw new RuntimeException("No object layers found in map object");
+    }
+
+    foreach (var ol in objLayers)
+    {
+      if (ol.m_TiledName == "Object Layer 1")
+      {
+        parseLevelObjects(ol);
+      }
+    }
+  }
+
+  void parseLevelObjects(SuperObjectLayer objLayer)
+  {
+    Debug.Log("We are parsing an object layer");
+    var objects = objLayer.gameObject.GetComponentsInChildren<SuperObject>();
         int playerId = 1;
         foreach (var superObject in objects)
-		{
-			var custProps = superObject.gameObject.GetComponent<SuperCustomProperties>();
-            
-			foreach (var prop in custProps.m_Properties)
-			{
-				if (prop.m_Name == "ObjectName")
-				{
-					switch (prop.m_Value)
-					{
-						case "player1Spawn":
-							//break;
-						case "player2Spawn":
-							//break;
-						case "player3Spawn":
-							//break;
-						case "player4Spawn":
-							//break;
-						case "button":
-							Debug.Log("We maky da player frum button");
-							var newPlayer = Instantiate(playerTemplate, superObject.transform.position, Quaternion.identity);
-                            newPlayer.GetComponent<PlayerIdComponent>().setPlayerId(playerId);
-                            playerId++;
-                            							
-							newPlayer.GetComponent<PlayerSpriteSetter>().SetPlayerNumber(playerId);
-                            Destroy(superObject.gameObject);
-							break;
-						default:
-							throw new RuntimeException("Unrecognized 'ObjectName' " + prop.m_Name + " found");
-					}
-				}
-			}
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    {
+      var custProps = superObject.gameObject.GetComponent<SuperCustomProperties>();
+
+      foreach (var prop in custProps.m_Properties)
+      {
+        if (prop.m_Name == "ObjectName")
+        {
+          switch (prop.m_Value)
+          {
+            case "player1Spawn":
+              //break;
+            case "player2Spawn":
+              //break;
+            case "player3Spawn":
+              //break;
+            case "player4Spawn":
+              //break;
+            case "button":
+              Debug.Log("We maky da player frum button");
+              SpawnPlayer(superObject, 3);
+              break;
+            default:
+              throw new RuntimeException("Unrecognized 'ObjectName' " + prop.m_Name + " found");
+          }
+        }
+      }
+    }
+  }
+
+  void SpawnPlayer(SuperObject superObject, int playerNumber)
+  {
+    var newPlayer = Instantiate(playerTemplate, superObject.transform.position, Quaternion.identity);
+    newPlayer.GetComponent<PlayerSpriteSetter>().SetPlayerNumber(playerNumber);
+  }
+
+  // Update is called once per frame
+  void Update () {
+
+  }
 }
