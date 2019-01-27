820e7aa5972c9c7218ba2d854538f1e7b53af416
Luke Fisher <lukewfisher@msn.com>
2019-01-26 19:13:03 -0700

    added game state manager for objectives/ win conditions

---
+++ Assets/Scripts/Components/ObjectiveComponent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Interactables;
+
+public class ObjectiveComponent : AbstractInteractable {
+    public bool isComplete = false;
+
+	void Start () {
+        GameStateManager.getLocalReference().Register(this);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public override void Interact(GameObject interactee)
+    {
+        //todo: check to see if this is the defender or attacker
+        isComplete = true;
+    }
+
+    public override void Disconnect()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Trigger()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Managers/GameStateManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour {
+    public int winThreshold = 2;
+    public float gameTime = 60.0f;
+    private const string gameObjectName = "GameStateManagerGameObject";
+    private List<Component> winObjectives = new List<Component>();
+    [SerializeField]
+    private int registeredGameObjects = 0;
+    [SerializeField]
+    private int activatedGameObjects = 0;
+
+    void Update () {        
+        int completedObjectives = 0;
+        gameTime -= Time.deltaTime;
+
+        if (gameTime < 0)
+        {
+            GameOver(); //Seeker Wins
+        }
+
+        foreach(ObjectiveComponent comp in winObjectives)
+        {
+            if (comp.isComplete)
+            {
+                completedObjectives++;
+                activatedGameObjects++;
+            }
+        }
+
+        if(completedObjectives >= winThreshold)
+        {
+            GameOver(); //Hiders Win
+        }
+    }
+
+    public void Register(Component comp)
+    {
+        winObjectives.Add(comp);
+        registeredGameObjects = winObjectives.Count;
+    }
+
+    public static GameStateManager getLocalReference()
+    {
+        GameObject gameObj = GameObject.Find(gameObjectName);
+        if (gameObj == null)
+            throw new System.Exception("Game State Manager game object reference not found.");
+            
+        return gameObj.GetComponent<GameStateManager>();
+    }
+
+    private void GameOver()
+    {
+
+    }
+}
