23aa8fa1352520e6cbeca6bca3789c8cf3aa55c9
mwingfield <mwingfie@gmail.com>
2019-01-27 15:21:42 -0700

    don't add Debug scenes to the build file

--- Assets/Scripts/Constants/Scenes.cs
+++ Assets/Scripts/Constants/Scenes.cs
@@ -2,53 +2,21 @@ using System;
 public class Scenes
 {
 	public const string DontDestroyOnLoad = "DontDestroyOnLoad";
-	public const string TitleScreen = "TitleScreen";
-	public const string Credits = "Credits";
-	public const string BitdecaySplash = "BitdecaySplash";
-	public const string GameJamSplash = "GameJamSplash";
-	public const string DebugJiggle = "DebugJiggle";
-	public const string DebugDash = "DebugDash";
-	public const string DebugDoor = "DebugDoor";
 	public const string Logan = "Logan";
-	public const string DebugPlayerMovement = "DebugPlayerMovement";
-	public const string DebugQuicktime = "DebugQuicktime";
+	public const string Credits = "Credits";
 	public enum SceneEnum
 	{
-		TitleScreen = 98,
-		Credits = 206,
-		BitdecaySplash = 144,
-		GameJamSplash = 253,
-		DebugJiggle = 57,
-		DebugDash = 103,
-		DebugDoor = 123,
 		Logan = 241,
-		DebugPlayerMovement = 159,
-		DebugQuicktime = 147,
+		Credits = 206,
 	}
 	public static string GetSceneNameFromEnum(SceneEnum sceneEnum)
 	{
 		switch (sceneEnum)
 		{
-			case SceneEnum.TitleScreen:
-				return TitleScreen;
-			case SceneEnum.Credits:
-				return Credits;
-			case SceneEnum.BitdecaySplash:
-				return BitdecaySplash;
-			case SceneEnum.GameJamSplash:
-				return GameJamSplash;
-			case SceneEnum.DebugJiggle:
-				return DebugJiggle;
-			case SceneEnum.DebugDash:
-				return DebugDash;
-			case SceneEnum.DebugDoor:
-				return DebugDoor;
 			case SceneEnum.Logan:
 				return Logan;
-			case SceneEnum.DebugPlayerMovement:
-				return DebugPlayerMovement;
-			case SceneEnum.DebugQuicktime:
-				return DebugQuicktime;
+			case SceneEnum.Credits:
+				return Credits;
 			default:
 				throw new Exception("Unable to resolve scene name for: " + sceneEnum);
 		}
