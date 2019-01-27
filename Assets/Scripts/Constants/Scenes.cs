ec16fb7fd85fcf63ef7cc5d41fea6828cac6abab
mwingfield <mwingfie@gmail.com>
2019-01-27 15:48:17 -0700

    still trying to get the build working

--- Assets/Scripts/Constants/Scenes.cs
+++ Assets/Scripts/Constants/Scenes.cs
@@ -2,25 +2,33 @@ using System;
 public class Scenes
 {
 	public const string DontDestroyOnLoad = "DontDestroyOnLoad";
-	public const string Logan = "Logan";
-	public const string Credits = "Credits";
 	public const string TitleScreen = "TitleScreen";
+	public const string Credits = "Credits";
+	public const string Logan = "Logan";
+	public const string DefenderLoses = "DefenderLoses";
+	public const string DefenderWins = "DefenderWins";
 	public enum SceneEnum
 	{
-		Logan = 241,
-		Credits = 206,
 		TitleScreen = 98,
+		Credits = 206,
+		Logan = 241,
+		DefenderLoses = 35,
+		DefenderWins = 190,
 	}
 	public static string GetSceneNameFromEnum(SceneEnum sceneEnum)
 	{
 		switch (sceneEnum)
 		{
-			case SceneEnum.Logan:
-				return Logan;
-			case SceneEnum.Credits:
-				return Credits;
 			case SceneEnum.TitleScreen:
 				return TitleScreen;
+			case SceneEnum.Credits:
+				return Credits;
+			case SceneEnum.Logan:
+				return Logan;
+			case SceneEnum.DefenderLoses:
+				return DefenderLoses;
+			case SceneEnum.DefenderWins:
+				return DefenderWins;
 			default:
 				throw new Exception("Unable to resolve scene name for: " + sceneEnum);
 		}
