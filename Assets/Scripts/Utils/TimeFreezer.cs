3c6bf853b0b667824ade041494e2070fb5926440
Tanner <Shadoninja@gmail.com>
2019-01-26 23:49:57 -0700

    Time freezer added

--- Assets/Scripts/Utils/TimeFreezer.cs
+++ Assets/Scripts/Utils/TimeFreezer.cs
@@ -1,18 +1,39 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class TimeFreezer : MonoBehaviour
 {
-    public bool FreezeTime;
+    private static string timeFreezerGameObjectName = "TimeFreezerManager";
 
+    public static TimeFreezer GetLocalReference()
+    {
+        GameObject timeFreezerGameObject = GameObject.Find(timeFreezerGameObjectName);
+        if (timeFreezerGameObject == null)
+        {
+            throw new Exception(String.Format("Unable to find {0}", timeFreezerGameObjectName));
+        }
+
+        return timeFreezerGameObject.GetComponent<TimeFreezer>();
+    }
+    
     private void Update()
     {
-        if (FreezeTime)
+        if (Input.GetKeyDown(KeyCode.K))
         {
-            Time.timeScale = 0;
+            StartCoroutine(PauseBriefly());
         }
-        else
-        {
-            Time.timeScale = 1;
-        }
+    }
+
+    public void TriggerHitStun()
+    {
+        StartCoroutine(PauseBriefly());
+    }
+    
+    IEnumerator PauseBriefly() 
+    {
+        Time.timeScale = 0;   
+        yield return new WaitForSecondsRealtime(.15f);
+        Time.timeScale = 1;     
     }
 }
