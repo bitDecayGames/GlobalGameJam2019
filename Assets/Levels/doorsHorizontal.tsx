3ce72ae3339ed562eba4b957225ea644d36b6fc2
nandemoguy <nandemoguy@gmail.com>
2019-01-27 10:27:09 -0700

    red doors

--- Assets/Levels/doorsHorizontal.tsx
+++ Assets/Levels/doorsHorizontal.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="doorsHorizontal" tilewidth="32" tileheight="16" tilecount="4" columns="2">
- <image source="Sprites/doorsHorizontal.png" width="64" height="32"/>
+<tileset version="1.2" tiledversion="1.2.1" name="doorsHorizontal" tilewidth="32" tileheight="16" tilecount="8" columns="2">
+ <image source="Sprites/doorsHorizontal.png" width="64" height="64"/>
  <tile id="0">
   <properties>
    <property name="ObjectName" value="horizontalHingeLeft"/>
@@ -32,5 +32,25 @@
   <objectgroup draworder="index">
    <object id="1" x="0" y="5" width="32" height="5"/>
   </objectgroup>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="ObjectName" value="horizontalHingeLeft"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="ObjectName" value="horizontalHingeRight"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="ObjectName" value="horizontalHingeLeft"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="ObjectName" value="horizontalHingeRight"/>
+  </properties>
  </tile>
 </tileset>
